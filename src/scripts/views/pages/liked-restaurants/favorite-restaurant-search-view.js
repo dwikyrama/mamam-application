02cd9533ed83932e217ed65b7d3236a34833f40b
dwikyrama <theprisonerofazkaban17@gmail.com>
2021-07-18 04:32:35 +0700

    Integrated the test to template creator
    Added favorite restaurant show specification and show-presenter.

--- src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-view.js
+++ src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-view.js
@@ -1,14 +1,26 @@
+import { createRestaurantItemTemplate } from '../../templates/template-creator'
+
 class FavoriteRestaurantSearchView {
   getTemplate () {
     return `
-      <div id="restaurant-search-container">
-          <input id="query" type="text">
-          <div class="restaurant-result-container">
-              <ul class="restaurants">
-              </ul>
-          </div>
-      </div>
-      `
+        <div id="restaurant-search-container">
+        <input id="query" type="text">
+        <div class="restaurant-result-container">
+            <ul class="restaurants">
+            </ul>
+        </div>
+    </div>
+    `
+  }
+
+  getFavoriteRestaurantTemplate() {
+    return `
+    <div class="content">
+        <h2 class="content__heading">Your Liked Restaurant</h2>
+        <div id="restaurants" class="restaurants">
+        </div>
+    </div>
+    `
   }
 
   runWhenUserIsSearching (callback) {
@@ -40,6 +52,19 @@ class FavoriteRestaurantSearchView {
 
     document.getElementById('restaurant-search-container')
       .dispatchEvent(new Event('restaurants:searched:updated'))
+  }
+
+  showFavoriteRestaurants(restaurants = []) {
+    let html;
+    if (restaurants.length) {
+      html = restaurants.reduce((carry, restaurant) => carry.concat(createRestaurantItemTemplate(restaurant)), '');
+    } else {
+      html = '<div class="restaurant-item__not__found"></div>';
+    }
+
+    document.getElementById('restaurants').innerHTML = html;
+
+    document.getElementById('restaurants').dispatchEvent(new Event('restaurants:updated'));
   }
 }
 
