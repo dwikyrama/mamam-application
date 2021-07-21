6747ddaea368fee69cde0bffe9f34555c46b99ec
dwikyrama <theprisonerofazkaban17@gmail.com>
2021-07-21 12:11:50 +0700

    Added E2E test to cancel liking a restaurant.

---
+++ e2e/unliking_restaurants.spec.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+const assert = require('assert')
+
+Feature('Unliking Restaurants')
+
+Before(({ I }) => {
+  I.amOnPage('/#/favorite')
+})
+
+Scenario('Unliking restaurants', async ({ I }) => {
+  I.see('Tidak ada restoran untuk ditampilkan', '.restaurant-item__not__found')
+
+  I.amOnPage('/')
+
+  I.seeElement('.post-item__name')
+  const firstRestaurant = locate('.post-item__name').first()
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant)
+  I.click(firstRestaurant)
+
+  I.seeElement('#likeButton')
+  I.click('#likeButton')
+
+  I.amOnPage('/#/favorite')
+  I.seeElement('.post-item__name')
+  const likedRestaurant = locate('.post-item__name').first()
+  const likedRestaurantName = await I.grabTextFrom('.post-item__name')
+
+  assert.strictEqual(firstRestaurantName, likedRestaurantName)
+
+  I.click(likedRestaurant)
+
+  I.seeElement('#likeButton')
+  I.click('#likeButton')
+
+  I.amOnPage('/#/favorite')
+
+  I.see('Tidak ada restoran untuk ditampilkan', '.restaurant-item__not__found')
+})
